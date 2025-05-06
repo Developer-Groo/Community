7d3d5a597452d94137184d45f40a63ed48449d16
Groo <woohyeon3382@naver.com>
2025-05-06 15:25:46 +0900

    feat: UserService & UserRepository 구현

---
+++ src/main/java/org/example/user/application/UserService.java
@@ -0,0 +1,26 @@
+package org.example.user.application;
+
+import org.example.user.application.dto.CreateUserRequestDto;
+import org.example.user.application.interfaces.UserRepository;
+import org.example.user.domain.User;
+import org.example.user.domain.UserInfo;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(CreateUserRequestDto dto) {
+        UserInfo userInfo = new UserInfo(dto.name(), dto.profileImageUrl());
+        User user = new User(null, userInfo);
+        return userRepository.save(user);
+    }
+
+    public User getUser(Long userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(IllegalArgumentException::new);
+    }
+}
